6e3f0818505f64d46d6f190d8f4c8aa18d409553
John Yeo <johnyeocx@gmail.com>
2021-03-17 19:31:44 +0800

    server v2

--- router.js
+++ router.js
@@ -1,7 +1,44 @@
 const express = require("express");
-const { extractDetailsFromPage } = require("./webScraper.js");
+// const { extractDetailsFromPage } = require("./webScraper.js");
 
 const router = express.Router();
+
+const puppeteer = require("puppeteer");
+const { pageEvaluator } = require("patang");
+
+async function extractDetailsFromPage(url) {
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--no-zygote",
+      "--no-default-browser-check",
+      "--bwsi",
+      "--disable-dev-shm-usage",
+      "--disable-infobars",
+      "--hide-scrollbars",
+    ],
+  });
+
+  const page = await browser.newPage();
+
+  //   await page.goto(exampleFlpktUrl, { waitUntil: "networkidle0" });
+  //   console.log("Flipkart Product Details");
+  //   console.log(await pageEvaluator.evaluateProductDetails(page, "flipkart"));
+  try {
+    await page.goto(url, { waitUntil: "networkidle0" });
+    const result = await pageEvaluator.evaluateProductDetails(page, "amazon");
+    console.log(result);
+    await page.close();
+    await browser.close();
+    return result;
+  } catch (error) {
+    console.log("unable to fetch from URL");
+    return -1;
+  }
+}
 
 router.get("/", (req, res) => {
   res.send("server is up and running");
