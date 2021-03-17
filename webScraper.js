d65dea2ced1e0c9a59a6468993b9627c0246a5cd
johnyeocx <51376134+johnyeocx@users.noreply.github.com>
2021-03-17 22:01:52 +0800

    Update webScraper.js

--- webScraper.js
+++ webScraper.js
@@ -2,3 +2,6 @@
 
 // invoking the function
 // extractDetailsFromPage("exampleAmznUrl").then((res) => console.log(res));
+
+//currently not used: implemented function in router.js to make it easier to read
+
